733b3fa40f45a2de510a2ad6bacd9dcc16a89c92
Ayush Srivatav <52977142+ayushh86@users.noreply.github.com>
2021-07-06 21:18:14 +0530

    Add files via upload

---
+++ questions2.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is capital of India ?",
+    answer: "Delhi",
+    options: [
+      "Lucknow",
+      "Delhi",
+      "UP",
+      "MP"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Capital of Pakistan?",
+    answer: "Islamabad",
+    options: [
+      "Lahore",
+      "Kabul",
+      "Islamabad",
+      "Karachi"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Where is statue of liberty situated?",
+    answer: "New York",
+    options: [
+      "Austin",
+      "New York",
+      "Chicago",
+      "Denver"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Where is Eiffel Tower located?",
+    answer: "France",
+    options: [
+      "India",
+      "Pakistan",
+      "America",
+      "France"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Where is Statue of Unity located?",
+    answer: "India",
+    options: [
+      "USA",
+      "India",
+      "Bhutan",
+      "Nepal"
+    ]
+  },
+  ];
---
+++ questions3.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "How many elements are there in a triangle?",
+    answer: "6",
+    options: [
+      "3",
+      "6",
+      "2",
+      "1"
+    ]
+  },
+    {
+    numb: 2,
+    question: "If two sides of a triangle are not equal, the triangle is called?",
+    answer: "scalene",
+    options: [
+      "scalene",
+      "acute",
+      "right angle",
+      "none"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many medians can a triangle have??",
+    answer: "3",
+    options: [
+      "1",
+      "5",
+      "2",
+      "3"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many altitudes can a triangle have??",
+    answer: "3",
+    options: [
+      "3",
+      "2",
+      "0",
+      "8"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The total measure of the three angles of a triangle is",
+    answer: "180",
+    options: [
+      "60",
+      "180",
+      "90",
+      "240"
+    ]
+  },
+  ];
